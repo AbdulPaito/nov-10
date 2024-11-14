a7f46ccb5dfd7f1daced3a1a56b184060c4db981
ABDUL PAITO <jiwoomark19@gmail.com>
2024-11-14 11:15:21 +0800

    Add files via upload

--- registration.php
+++ registration.php
@@ -172,7 +172,7 @@ button:hover {
 <body>
 <section id="registration-section">
     <div class="container">
-        <h1>Registration</h1>
+        <h1>Trainee Information</h1>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <form method="GET" action="dashboard.php">
         <input type="hidden" name="page" value="registration">
