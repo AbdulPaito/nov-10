5426b4eb5114cabc09d6f72e326a1761d88948c1
ABDUL PAITO <jiwoomark19@gmail.com>
2024-11-14 13:48:19 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -113,6 +113,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
+
+  <link rel="icon" href="TESDA icon.png" type="image/png">
   <link rel="stylesheet" href="log.css">
   <link rel="stylesheet" href="log.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
